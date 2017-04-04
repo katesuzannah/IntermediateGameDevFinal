328016b06d58304870ba30efe1ad3358a5aadc52
Unknown <jams.melnick@gmail.com>
2017-04-04 15:05:34 -0400

    Window/LookMove
    Window model and updated (functional) Look/Move Scripts

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
 
-	float upDownLook = 0f;
+
+	Vector3 moveVector;
+	public float moveSpeed = 5f;
 
 	// Use this for initialization
 	void Start () {
-		
 		Cursor.visible = false;
 		Cursor.lockState = CursorLockMode.Locked;
 	}
@@ -17,17 +18,13 @@ public class PlayerMove : MonoBehaviour {
 	void Update () {
 		//get mouse input data
 		float mouseX = Input.GetAxis ("Mouse X");
-		float mouseY = Input.GetAxis ("Mouse Y");
 
 		//rotate camera based on mouse movement
 		transform.Rotate(0f, mouseX * 3f, 0f);
 
-		//clamp/constrain the X angle so we can't look too far up or down
-		upDownLook -= mouseY;
-		upDownLook = Mathf.Clamp (upDownLook, -80f, 80f); //constrain look 80 degrees
-
-		//unroll the camera
-		transform.eulerAngles = new Vector3( upDownLook* 3f, transform.eulerAngles.y, 0f);
+		//movement
+		moveVector = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0, Input.GetAxis("Vertical") * moveSpeed);
+		transform.Translate(moveVector * Time.deltaTime);
 
 	}
 }
