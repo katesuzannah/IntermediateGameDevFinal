a79d5f1d5c8744b9488a3f2a409cf7db0bd2a763
Grant Wang <yidegrant@gmail.com>
2017-04-06 16:46:31 -0400

    Updated playerMove and fixed bookcase autorise issue

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
 
+    CharacterController playerCharCon;
 
 	Vector3 moveVector;
 	public float moveSpeed = 5f;
@@ -12,6 +13,7 @@ public class PlayerMove : MonoBehaviour {
 	void Start () {
 		Cursor.visible = false;
 		Cursor.lockState = CursorLockMode.Locked;
+        playerCharCon = GetComponent<CharacterController>();
 	}
 
 	// Update is called once per frame
@@ -22,9 +24,23 @@ public class PlayerMove : MonoBehaviour {
 		//rotate camera based on mouse movement
 		transform.Rotate(0f, mouseX * 3f, 0f);
 
-		//movement
-		moveVector = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0, Input.GetAxis("Vertical") * moveSpeed);
-		transform.Translate(moveVector * Time.deltaTime);
+        //movement
+        // moveVector = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0, Input.GetAxis("Vertical") * moveSpeed);
+        // transform.Translate(moveVector * Time.deltaTime);
+        float frontBack = Input.GetAxis("Vertical") * moveSpeed;
+        float leftRight = Input.GetAxis("Horizontal") * moveSpeed;
+        playerCharCon.Move(transform.forward * frontBack * Time.deltaTime);
+        playerCharCon.Move(transform.right * leftRight * Time.deltaTime);
 
-	}
+    }
+
+    void FixedUpdate()
+    {
+        playerCharCon.Move(Vector3.up * Time.deltaTime * -5f);
+    }
+
+    void playerGravity()
+    {
+        
+    }
 }
