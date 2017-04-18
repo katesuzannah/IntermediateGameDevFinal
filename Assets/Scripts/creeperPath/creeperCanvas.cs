ad436d0d7aa60a47e6c5f03eae649735057efeab
Snippy Nguyen <chevy666camaro@gmail.com>
2017-04-18 15:22:59 -0400

    Creeper Path paper is working
    the paper can access the username and the password from the login screen now

---
+++ Assets/Scripts/creeperPath/creeperCanvas.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class creeperCanvas : MonoBehaviour {
+
+	public Text usernameText;
+	public Text passwordText; 
+
+	// Use this for initialization
+	void Start () {
+		usernameText.text = LoginData.username;
+		passwordText.text = LoginData.password;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
