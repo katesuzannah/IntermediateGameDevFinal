5c5e9cf4efbf1d3ce1d880ff21b350ec1983a101
Grant Wang <yidegrant@gmail.com>
2017-04-18 14:34:44 -0400

    Political Send and Receive

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager Instance;
+
+    public GameObject worldCanvas;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        Instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
