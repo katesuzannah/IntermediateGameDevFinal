2b953e8a545a720c25b836f80bbb8a4caef4365e
Kate Smith <katesuzannah@gmail.com>
2017-05-03 19:59:15 -0400

    Added some audio to loginSnips, killedByIlluminati, and grantLevel

--- Assets/Scripts/secretPoliticalBook.cs
+++ Assets/Scripts/secretPoliticalBook.cs
@@ -9,11 +9,13 @@ public class secretPoliticalBook : MonoBehaviour {
     Vector3 originalPos;
     float maxDistance = 0.4f;
     bool pulled;
+	AudioSource zelda;
 
 	// Use this for initialization
 	void Start () {
         originalPos = transform.position;
         pulled = false;
+		zelda = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
@@ -21,6 +23,7 @@ public class secretPoliticalBook : MonoBehaviour {
         float dist = Vector3.Distance(transform.position, originalPos);
         if(dist > maxDistance && !pulled)
         {
+			zelda.Play ();
             bookcase.GetComponent<bookcase>().bookPulled = true;
             pulled = true;
             govRoom.SetActive(true);
